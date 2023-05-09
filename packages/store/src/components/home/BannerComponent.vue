b5f95985453289be9e77d00b08a6b1fac8238ec3
Daniel Vinicius <viniccius774@gmail.com>
2023-05-09 14:33:07 -0300

    conserta numero do botão

--- packages/store/src/components/home/BannerComponent.vue
+++ packages/store/src/components/home/BannerComponent.vue
@@ -10,7 +10,7 @@
 import Button from '../layout/Button.vue';
 
 const sendWppMessage = () => {
-    const phone = '5524993954501';
+    const phone = '5521981834355';
     const now = new Date().getHours();
     const greeting = now >= 5 && now < 12 ? 'Bom dia!' : now >= 12 && now < 18 ? 'Boa tarde!' : 'Boa noite!';
     const text = `${greeting} Eu gostaria de agendar uma hospedagem para o meu pet.`
